586ee20581d28911fe86b753410350d2b1362c86
Victor Escandiel <joaoescandielb@gmail.com>
2023-12-14 17:26:25 -0300

    feat: title component in home page done

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -3,7 +3,7 @@ import styles from "./Rodape.module.css";
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <h2>Desenvolvido por Victor Escandiel</h2>
+      <h2>Developed by Victor Escandiel within Alura Escola de Tecnologia</h2>
     </footer>
   );
 }
