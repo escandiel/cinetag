44eb17895ffd3be8a602e12a30ec3480b5ec5753
Victor Escandiel <joaoescandielb@gmail.com>
2024-01-02 22:46:36 -0300

    fix: importing useFavoritoContext to the Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,22 @@
+import { useFavoritoContext } from "contexts/Favoritos";
 import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.svg";
 
 export default function Card({ id, titulo, capa }) {
+  const { favorito, adicionarFavorito } = useFavoritoContext();
+  // const ehFavorito = favorito.some((fav) => fav.id === id);
+
   return (
     <div className={styles.container}>
       <img src={capa} alt={titulo} className={styles.capa} />
       <h2>{titulo}</h2>
-      <img src={iconeFavoritar} className={styles.favoritar} />
+      <img
+        src={iconeFavoritar}
+        className={styles.favoritar}
+        onClick={() => {
+          adicionarFavorito({ id, titulo, capa });
+        }}
+      />
     </div>
   );
 }
